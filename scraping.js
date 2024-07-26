af33a48a6996931d48acf1c7865da8d2e12d268b
nicolasqdev <nicolas.queirolo@ace-agency.io>
2024-07-26 17:04:06 +0200

    add express

---
+++ scraping.js
@@ -0,0 +1,88 @@
+//import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+
+const scraping = async (res) => {
+    
+    //proxy server
+    const SBR_WS_ENDPOINT = 'wss://brd-customer-hl_15810fe2-zone-scraping_browser2:25j1lhqe70n1@brd.superproxy.io:9222';
+
+
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({headless: true, browserWSEndpoint: SBR_WS_ENDPOINT});
+    const page = await browser.newPage();
+    
+
+    await page.goto('https://www.google.com/',{waitUntil: "networkidle0", timeout: 90000});
+
+    const element = await page.waitForSelector('#L2AGLb');//accepter conditio utilisateur
+    
+    await element.click(); 
+
+    await page.evaluate(() => {
+        document.querySelector(".gLFyf").value = "contrat électricité pas cher";//champs recherche
+    });
+
+    await page.waitForSelector('.gLFyf');
+    await page.click(".gLFyf");
+
+    
+    //const searchResultSelector = '.gNO89b';// class css button lancer la recherche gNO89b
+    
+    //await page.waitForSelector('.gNO89b');
+    //await page.click('.gNO89b');
+    // faire des tickets avec google ads pour faire 
+    // 
+    page.$eval(`.gNO89b`, element =>
+        element.click()
+      );
+
+    await page.waitForSelector('.gLFyf');
+
+    await page.waitForSelector('.sVXRqc');
+
+
+    //link page
+    
+    //console.log(hrefsPageLink);
+    //link page
+
+
+    //page 1 
+    const hrefs = await page.$$eval(
+        ".sVXRqc",
+        els => els.map(el => el.href)
+      );
+      console.log("page 1")
+    console.log(hrefs);
+    //console.log("fini");
+    await page.waitForSelector('.fl');
+    await page.click('.fl');
+
+     //page 2
+    await page.waitForSelector('.sVXRqc');
+    const hrefs2 = await page.$$eval(
+      ".sVXRqc",
+      els => els.map(el => el.href)
+    );
+    console.log("page 2")
+    console.log(hrefs2);
+
+
+
+    await page.waitForSelector('.fl');
+    await page.click('.fl');
+
+    //Page 3
+    await page.waitForSelector('.sVXRqc');
+    const hrefs3 = await page.$$eval(
+      ".sVXRqc",
+      els => els.map(el => el.href)
+    );
+    console.log("page 3")
+    console.log(hrefs3);
+    const results = [hrefs,hrefs2,hrefs3]; 
+    res.send(results)
+    await browser.close();
+}
+
+module.exports = {scraping};
--- server.js
+++ server.js
@@ -1,6 +1,97 @@
-import puppeteer from 'puppeteer';
+//import express from 'express';
+const express = require('express');
+const puppeteer = require('puppeteer');
+const {scraping} = require('./scraping.js');
+const app = express();
 
-(async () => {
+app.listen(3000, () => {
+  console.log('hello world');
+})
+
+app.get('/scraping', async (req,res) => {
+  //proxy server
+  const SBR_WS_ENDPOINT = 'wss://brd-customer-hl_15810fe2-zone-scraping_browser2:25j1lhqe70n1@brd.superproxy.io:9222';
+
+
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({headless: true, browserWSEndpoint: SBR_WS_ENDPOINT});
+  const page = await browser.newPage();
+  
+
+  await page.goto('https://www.google.com/',{waitUntil: "networkidle0", timeout: 90000});
+
+  const element = await page.waitForSelector('#L2AGLb');//accepter conditio utilisateur
+  
+  await element.click(); 
+
+  await page.evaluate(() => {
+      document.querySelector(".gLFyf").value = "contrat électricité pas cher";//champs recherche
+  });
+
+  await page.waitForSelector('.gLFyf');
+  await page.click(".gLFyf");
+
+  
+  //const searchResultSelector = '.gNO89b';// class css button lancer la recherche gNO89b
+  
+  //await page.waitForSelector('.gNO89b');
+  //await page.click('.gNO89b');
+  // faire des tickets avec google ads pour faire 
+  // 
+  page.$eval(`.gNO89b`, element =>
+      element.click()
+    );
+
+  await page.waitForSelector('.gLFyf');
+
+  await page.waitForSelector('.sVXRqc');
+
+
+  //link page
+  
+  //console.log(hrefsPageLink);
+  //link page
+
+
+  //page 1 
+  const hrefs = await page.$$eval(
+      ".sVXRqc",
+      els => els.map(el => el.href)
+    );
+    console.log("page 1")
+  console.log(hrefs);
+  //console.log("fini");
+  await page.waitForSelector('.fl');
+  await page.click('.fl');
+
+   //page 2
+  await page.waitForSelector('.sVXRqc');
+  const hrefs2 = await page.$$eval(
+    ".sVXRqc",
+    els => els.map(el => el.href)
+  );
+  console.log("page 2")
+  console.log(hrefs2);
+
+
+
+  await page.waitForSelector('.fl');
+  await page.click('.fl');
+
+  //Page 3
+  await page.waitForSelector('.sVXRqc');
+  const hrefs3 = await page.$$eval(
+    ".sVXRqc",
+    els => els.map(el => el.href)
+  );
+  console.log("page 3")
+  console.log(hrefs3);
+  const results = [hrefs,hrefs2,hrefs3]; 
+  res.send(results)
+  await browser.close();
+})
+
+/* (async () => {
   //proxy server
     const SBR_WS_ENDPOINT = 'wss://brd-customer-hl_15810fe2-zone-scraping_browser2:25j1lhqe70n1@brd.superproxy.io:9222';
 
@@ -9,11 +100,7 @@ import puppeteer from 'puppeteer';
     const browser = await puppeteer.launch({headless: true, browserWSEndpoint: SBR_WS_ENDPOINT});
     const page = await browser.newPage();
     
-    // Navigate the page to a URL
-   /*  await page.goto('https://www.mon-ip.com/',{waitUntil: "networkidle0", timeout: 90000});
-    return; */
 
-    
     await page.goto('https://www.google.com/',{waitUntil: "networkidle0", timeout: 90000});
 
     const element = await page.waitForSelector('#L2AGLb');//accepter conditio utilisateur
@@ -83,4 +170,4 @@ import puppeteer from 'puppeteer';
     console.log("page 3")
     console.log(hrefs3);
     await browser.close();
-})();
+})(); */
